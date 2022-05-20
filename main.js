86b14fca2c352497ef7434821d35c57b55f7b8bf
MarcosCruzOff <marcoscruz_off@outlook.com>
2022-05-20 16:46:16 -0300

    Dashboard Admin Finished

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {    
+    $('#example').DataTable({        
+        language: {
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "Resultados não encontrados",
+                "info": "Mostrando registros do _START_ a _END_ de um total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros de 0 a 0 de um total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast":"Ultimo",
+                    "sNext":"Próximo",
+                    "sPrevious": "Anterior"
+			     },
+			     "sProcessing":"Procesando...",
+            },
+        //para usar los botones   
+        responsive: "true",
+        dom: 'Bfrtilp',       
+        buttons:[ 
+			{
+				extend:    'excelHtml5',
+				text:      '<i class="fas fa-file-excel"></i> ',
+				titleAttr: 'Exportar a Excel',
+				className: 'btn btn-success'
+			},
+			{
+				extend:    'pdfHtml5',
+				text:      '<i class="fas fa-file-pdf"></i> ',
+				titleAttr: 'Exportar a PDF',
+				className: 'btn btn-danger'
+			},
+			{
+				extend:    'print',
+				text:      '<i class="fa fa-print"></i> ',
+				titleAttr: 'Imprimir',
+				className: 'btn btn-info'
+			},
+		]	        
+    });     
+});
